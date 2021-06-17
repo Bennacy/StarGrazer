20fca4daa6a3c64c2237b1905f9411be132fa6d9
Bennacy <benjamimrodrigues.mg@gmail.com>
2021-06-17 15:24:43 +0100

    just a button

--- Public/sketch.js
+++ Public/sketch.js
@@ -1,276 +1,41 @@
-let addBtn
-let cv
-let playerCard
-let playerArr=[]
-let grid=[]
-let value=0
-let gridSize=15
-let totalPlayer
-let squareCycle
+let button
 
 function setup() {
-  playerCard=''
-  totalPlayer=4
-  squareCycle=1
   cv=createCanvas(720, 720);
   cv.position(windowWidth/2-width/2, windowHeight/2-height/2)
-  // addBtn= new Button(0,0, 100,50, 210,50,120, addStuff, 'Increase')
-
-  for(let i=0; i<width/gridSize; i++){
-    grid[i]=[]
-    for(let j=0; j<height/gridSize; j++){
-      grid[i][j]= new Grid(i,j)
-      grid[i][j].draw_grid()
-    }
-  }
-  push()
-    strokeWeight(3)
-    stroke('red')
-
-    line(width/2 - 1*gridSize, height/2 - 1*gridSize, width/2 - 1*gridSize, height/2 + 1*gridSize)
-    line(width/2 - 1*gridSize, height/2 - 1*gridSize, width/2 + 1*gridSize, height/2 - 1*gridSize)
-
-    line(width/2 + 1*gridSize, height/2 - 1*gridSize, width/2 + 1*gridSize, height/2 + 1*gridSize)
-    line(width/2 + 1*gridSize, height/2 + 1*gridSize, width/2 - 1*gridSize, height/2 + 1*gridSize)
-  pop()
+  button= new Button(0,0, 200,75, 0,171,235, btn, 'Increase')
 }
 
+
 function draw() {
-  // addBtn.mouse_over()
-  // addBtn.draw_button()
+  background(255)
 
-  if(playerCard!=''){
-    playerCard.visitBtn.mouse_over()
-    playerCard.visitBtn.draw_button()
-  }
-
-
+  button.mouse_over()
+  button.draw_button()
 }
 
 function keyPressed(){
-  switch (keyCode) {
-    case (81):
-      playerArr=[]
-      clear()
-      setup()
-      for(let i=0; i<width/gridSize*2-4; i++)
-        setTimeout(function(){
-          addStuff()
-        }, 250)
-        
-      break;
-
-    case 69:
-      playerArr=[]
-      clear()
-      setup()
-      break
-
-    case 87:
-      addStuff()
-      break
-  }
+  
 }
 
 function mousePressed(){
-  // if(addBtn.mouse_over()){
-  //   addBtn.mouse_pressed()
-  // }
-
-  for(let i=0; i<playerArr.length; i++){
-    if(playerArr[i].mouse_over() && playerCard==''){
-      playerArr[i].mouse_pressed()
-    }
-  }
-  
-  if(playerCard!=''){
-    
-    if(playerCard.visitBtn.mouse_over()){
-      playerCard.visitBtn.mouse_pressed()
-    }
-
-    playerCard.over_x()
+  if(button.mouse_over()){
+    button.mouse_pressed()
   }
 }
 
-function addStuff(){
-  totalPlayer++
-
-  // let pX= (value % (width/gridSize))
-  // let pY= Math.floor(value / (width/gridSize))
-
-  if(Math.ceil(totalPlayer/4)>squareCycle)
-  {
-    squareCycle++
-    push()
-      strokeWeight(2)
-      stroke('red')
-
-      // line(width/2 - Math.ceil(totalPlayer/4)*gridSize, height/2 - Math.ceil(totalPlayer/4)*gridSize, width/2 - Math.ceil(totalPlayer/4)*gridSize, height/2 + Math.ceil(totalPlayer/4)*gridSize)
-      // line(width/2 - Math.ceil(totalPlayer/4)*gridSize, height/2 - Math.ceil(totalPlayer/4)*gridSize, width/2 + Math.ceil(totalPlayer/4)*gridSize, height/2 - Math.ceil(totalPlayer/4)*gridSize)
-
-      // line(width/2 + Math.ceil(totalPlayer/4)*gridSize, height/2 - Math.ceil(totalPlayer/4)*gridSize, width/2 + Math.ceil(totalPlayer/4)*gridSize, height/2 + Math.ceil(totalPlayer/4)*gridSize)
-      // line(width/2 + Math.ceil(totalPlayer/4)*gridSize, height/2 + Math.ceil(totalPlayer/4)*gridSize, width/2 - Math.ceil(totalPlayer/4)*gridSize, height/2 + Math.ceil(totalPlayer/4)*gridSize)
-    pop()
-}
-
-  placeNew()
-
-  playerArr[totalPlayer-5].draw_player()
-}
-
-function placeNew(){
-  let sideVar= totalPlayer%4
-  let placeVar
-
-  switch (sideVar) {
-    case 1:
-      placeVar=(width/2 - squareCycle*gridSize)+(gridSize*int(random(squareCycle*2-1, 1)))
-      playerArr.push(new Player(placeVar, height/2 - Math.ceil(totalPlayer/4)*gridSize, sideVar))
-      break;
-    
-    case 2:
-      placeVar=(height/2 - squareCycle*gridSize)+(gridSize*int(random(squareCycle*2-1, 1)))
-      playerArr.push(new Player(width/2 + Math.ceil(totalPlayer/4)*gridSize -gridSize, placeVar, sideVar))
-      break;
-    
-    case 3:
-      placeVar=(width/2 - squareCycle*gridSize)+(gridSize*int(random(squareCycle*2-1, 1)))
-      playerArr.push(new Player(placeVar, height/2 + Math.ceil(totalPlayer/4)*gridSize -gridSize, sideVar))
-      break;
-
-    case 0:
-      placeVar=(height/2 - squareCycle*gridSize)+(gridSize*int(random(squareCycle*2-1, 1)))
-      playerArr.push(new Player(width/2 - Math.ceil(totalPlayer/4)*gridSize, placeVar, sideVar))
-      break;
+function mouseReleased(){
+  if(button.pressed==true){
+    button.mouse_released()
   }
 }
 
-function visitPlayer(){
-  print('Visiting')
+function btn(){
+  print('Unnecessary function')
 }
 
 
 
-
-
-
-
-
-
-class Grid{
-  constructor(x,y){
-    this.x=x
-    this.y=y
-  }
-  draw_grid(){
-    rect(this.x*gridSize,this.y*gridSize, gridSize,gridSize)
-  }
-}
-
-
-class Player{
-  constructor(x,y, side){
-    this.x=x
-    this.y=y
-    switch(side){
-      case 0:
-        this.side='Left'
-        break
-
-      case 1:
-        this.side='Up'
-        break
-
-      case 2:
-        this.side='Right'
-        break
-
-      case 3:
-        this.side='Down'
-        break
-    }
-    this.id=playerArr.length
-    this.test=side
-  }
-
-  draw_player(){
-    push()
-      fill("purple")
-      strokeWeight(1)
-      rect(this.x,this.y, gridSize,gridSize)
-    pop()
-  }
-
-  mouse_over(){
-    if(mouseX>this.x&&mouseX<this.x+gridSize && mouseY>this.y&&mouseY<this.y+gridSize){
-      return true
-    }else{
-      return false
-    }
-  }
-
-  mouse_pressed(){
-    // rect(this.x,this.y,200,300)
-    playerCard=new PlayerCard(this.x,this.y,this.id)
-
-    playerCard.draw_card()
-    playerCard.draw_x()
-  }
-}
-
-
-class PlayerCard{
-  constructor(x,y,pId){
-    this.x=x
-    this.y=y
-    this.w=200
-    this.h=300
-    this.pId=pId
-    
-    if(this.x+this.w>width){
-      this.x=this.x-this.w
-    }
-    if(this.y+this.h>height){
-      this.y=this.y-this.h
-    }
-
-    this.visitBtn= new Button(this.x+15,this.y+this.h-65, this.w-30,50, 190,190,190, visitPlayer, 'Visit')
-  }
-
-  draw_card(){    
-    rect(this.x,this.y,this.w,this.h)
-    push()
-      fill(0)
-      textAlign(CENTER, CENTER)
-      textSize(30)
-      strokeWeight(3)
-      text('Player '+(this.pId+1),this.x+this.w/2,this.y+this.h/2)
-    pop()
-  }
-
-  draw_x(){
-    push()
-      fill(255,0,0)
-      rect(this.x+(this.w-25),this.y, 25,25)
-      strokeWeight(3)
-      stroke(170,0,0)
-      fill(170,0,0)
-      textSize(20)
-      textAlign(CENTER, CENTER)
-      text('X',(this.x+(this.w-25))+25/2,this.y+28/2)
-    pop()
-  }
-
-  over_x(){
-    if(mouseX>this.x+(this.w-25)&&mouseX<this.x+this.w && mouseY>this.y&&mouseY<this.y+25){
-      setup()
-      for(let i=0; i<playerArr.length; i++){
-        playerArr[i].draw_player()
-      }
-    }
-  }
-}
 
 
 class Button{
@@ -288,17 +53,21 @@ class Button{
     this.g=this.origG
     this.b=this.origB
 
+    this.pressed=false
     this.text=text
+    this.strkWeight=5
 
   }
 
   draw_button(){
     push()
       fill(this.r,this.g,this.b)
-      strokeWeight(1.5)
+      strokeWeight(this.strkWeight)
+      stroke(this.r+20,this.g+20,this.b+20)
       textAlign(CENTER, CENTER)
-      rect(this.x,this.y,this.w,this.h)
+      rect(this.x,this.y,this.w,this.h,20,5,20,5)
       fill("black")
+      noStroke()
       text(this.text, this.x+this.w/2, this.y+this.h/2)
     pop()
   }
@@ -318,7 +87,15 @@ class Button{
   }
 
   mouse_pressed(index){
+    this.y+=5
+    this.pressed=true
+    this.strkWeight=0
+    this.func(index)
+  }
 
-      this.func(index)
+  mouse_released(){
+    this.pressed=false
+    this.strkWeight=5
+    this.y-=5
   }
 }
