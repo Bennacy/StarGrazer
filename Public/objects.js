a7d04e62ccc41e1e6551ef7070cce3aa73b93b10
Bennacy <benjamimrodrigues.mg@gmail.com>
2021-06-21 18:20:33 +0100

    Update objects.js

--- Public/objects.js
+++ Public/objects.js
@@ -266,12 +266,13 @@ class Mission{
 
 
 class Button{
-  constructor(x, y, w, h, r, g, b, func, text){
+  constructor(x, y, w, h, r, g, b, func, text, bType, cSide){
     this.x=x
     this.y=y
     this.w=w
     this.h=h
     this.func=func
+    this.bType=bType
     
     this.origR=r
     this.origG=g
@@ -280,28 +281,139 @@ class Button{
     this.g=this.origG
     this.b=this.origB
 
+    this.pressed=false
     this.text=text
+    this.cSide=cSide
 
+    switch(this.bType){
+      case 1:
+        this.clickDepth=2
+        this.strkWeight=2
+        this.clickStroke=0
+        this.stkDiff=-255
+
+        this.upL=10
+        this.upR=10
+        this.dnL=10
+        this.dnR=10
+
+        this.highlight1=-10
+        this.highlight2=25
+
+        break
+
+      case 2:
+        this.clickDepth=5
+        this.strkWeight=5
+        this.clickStroke=0
+        this.stkDiff=20
+
+        this.upL=35
+        this.upR=5
+        this.dnL=5
+        this.dnR=35
+
+        this.highlight1=10
+        this.highlight2=30
+        break
+
+      case 3:
+        this.clickDepth=10
+        this.strkWeight=5
+        this.clickStroke=1
+        this.stkDiff=20
+
+        this.upL=10000
+        this.upR=10000
+        this.dnL=10000
+        this.dnR=10000
+
+        this.highlight1
+        this.highlight2
+        break
+
+      case 4:
+        this.clickDepth=3
+        this.strkWeight=5
+        this.clickStroke=1
+        this.stkDiff=20
+
+        switch(this.cSide){ // Which side is slanted
+          case 1: // top left
+            this.upL=50
+            this.upR=5
+            this.dnL=5
+            this.dnR=5
+            break
+
+          case 2: // top right
+            this.upL=5
+            this.upR=50
+            this.dnL=5
+            this.dnR=5
+            break
+
+          case 3: // bottom left
+            this.upL=5
+            this.upR=5
+            this.dnL=50
+            this.dnR=5
+            break
+
+          case 4: // bottom right
+            this.upL=5
+            this.upR=5
+            this.dnL=5
+            this.dnR=50
+            break
+            
+          case 5: // none
+            this.upL=5
+            this.upR=5
+            this.dnL=5
+            this.dnR=5
+            break
+
+          default:
+              this.upL=50
+              this.upR=5
+              this.dnL=5
+              this.dnR=5
+
+        }
+
+        this.highlight1
+        this.highlight2
+        break
+    }
+    this.strkSave=this.strkWeight
   }
 
   draw_button(){
     push()
+      strokeWeight(this.strkWeight)
+      stroke(this.r+this.stkDiff, this.g+this.stkDiff, this.b+this.stkDiff)
       fill(this.r,this.g,this.b)
-      strokeWeight(1.5)
-      textAlign(CENTER, CENTER)
-      rect(this.x,this.y,this.w,this.h)
+      rect(this.x,this.y,this.w,this.h, this.upL,this.upR,this.dnR,this.dnL)
+      
+      noStroke()
+      textFont(font)
       fill("black")
+      textSize(this.w/this.text.length)
+      textAlign(CENTER, CENTER)
       text(this.text, this.x+this.w/2, this.y+this.h/2)
     pop()
   }
 
   mouse_over(){
     if(mouseX>this.x&&mouseX<this.x+this.w && mouseY>this.y&&mouseY<this.y+this.h){
-      this.r=this.origR-20
-      this.g=this.origG-20
-      this.b=this.origB-20
+      if(this.pressed==false && pressing==false){
+        this.r=this.origR-this.highlight1
+        this.g=this.origG-this.highlight1
+        this.b=this.origB-this.highlight1
+      }
       return true
-    }else{
+    }else if(this.pressed==false){
       this.r=this.origR
       this.g=this.origG
       this.b=this.origB
@@ -309,9 +421,25 @@ class Button{
     }
   }
 
-  mouse_pressed(index){
-
+  mouse_pressed(){
+    this.r=this.origR-this.highlight2
+    this.g=this.origG-this.highlight2
+    this.b=this.origB-this.highlight2
+    this.y+=this.clickDepth
+    this.pressed=true
+    this.strkWeight=this.clickStroke
+    pressing=true
+  }
+  
+  mouse_released(array,index){
+    
+    if(array[index].mouse_over()){
       this.func(index)
+    }
+    this.pressed=false
+    this.strkWeight=this.strkSave
+    this.y-=this.clickDepth
+    pressing=false
   }
 }
 
